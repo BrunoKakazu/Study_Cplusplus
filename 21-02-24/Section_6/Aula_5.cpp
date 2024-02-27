406061330c4d11821b50017d3461258a5947c853
Bruno Kakazu <brunokakazu98@gmail.com>
2024-02-27 19:30:36 -0300

    Continuando a aula

--- 21-02-24/Section_6/Aula_5.cpp
+++ 21-02-24/Section_6/Aula_5.cpp
@@ -28,6 +28,16 @@ int main()
 	long people_in_florida{ 20610000 };
 	cout << "There are about " << people_in_florida << " people in Florida" << endl;
 
+	// long people_in_earth = 7'600'000'000; -> Neste caso é apresentado uma coisa benéfica em realizar em inicialização em lista. Quando isto acontece, ao invés de dar erro, ele irá apresentar um número muito grande, porém negativo. Esse é uns dos erros típicos de overflow.
 	long long people_in_earth{ 7'600'000'000 }; // No caso de utilizar um compilador de c++14, caso os números sejam muito grandes, para nã se perder, ele permite que durante a marcação de números você utiliza ' e ele irá comilar normalmente. Neste exemplo, este número não como entrar em um tipo long, então ele deverá ser do tipo long long.
 	cout << "There are about " << people_in_earth << " people on earth" << endl;
+	
+	long long distance_to_alpha_centuri{ 9'461'000'000'000 };
+	cout << "The distance to alpha centuri is " << distance_to_alpha_centuri << " kilometers" << endl;
+
+	/**************************************************
+	* Floating point types
+	* *************************************************/
+
+
 }
